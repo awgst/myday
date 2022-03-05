a3cb4c4ab753c7dcc018a0cd3fa152b027d72e07
awgst <trisaktiawang@gmail.com>
2022-03-06 06:59:05 +0700

    Search fix bug not showing card with same name

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -33,47 +33,10 @@ class JsonController extends Controller
         foreach ($all as $value) {
             foreach ($value->card as $card) {
                 if ($card) {
-                    if (!in_array($card, array_column($response, 'value'))) {
-                        $response[] = ['id'=>$value->id, 'value'=>$card, 'category'=>$value->item];
-                    }
+                    $response[] = ['id'=>$value->id, 'value'=>$card, 'category'=>$value->item];
                 }
             }
         }
-        // dd($response);
-        // $all = DB::table(
-        //                 DB::raw('
-        //                     users
-        //                     full join items on items.user_id=users.id
-        //                     full join cards on cards.item_id=items.id
-        //                     full join tasks on tasks.card_id=cards.id
-        //                 ')
-        //             )
-        //             ->where(function ($query) use($search){
-        //                 return $query->where('items.name', 'ilike', '%'.$search.'%')
-        //                             ->orWhere('cards.name', 'ilike', '%'.$search.'%')
-        //                             ->orWhere('tasks.name', 'ilike', '%'.$search.'%');
-        //             })
-        //             ->select([
-        //                 'tasks.name as task',
-        //                 'cards.name as card', 
-        //                 'items.name as item',
-        //                 'items.id'
-        //             ])
-        //             ->get()
-        //             ->toArray();
-        // $response = [];
-        // foreach ($all as $value) {
-        //     if ($value->card) {
-        //         if (!in_array('• '.$value->card, array_column($response, 'value'))) {
-        //             $response[] = ['id'=>$value->id, 'value'=>'• '.$value->card, 'category'=>'# '.$value->item];
-        //         }
-        //     }
-        //     if ($value->task) {
-        //         if (!in_array('- '.$value->task, array_column($response, 'value'))) {
-        //             $response[] = ['id'=>$value->id, 'value'=>'- '.$value->task, 'category'=>'# '.$value->card.' in '.$value->item];
-        //         }
-        //     }
-        // }
         return response()->json($response);
     }
 }
