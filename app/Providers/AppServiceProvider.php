3e09e7515c0316f0aa5e50497beea2e130aeceb1
awgst <trisaktiawang@gmail.com>
2022-03-16 19:20:16 +0700

    Setting app_env on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Blade::component('component.item', 'item');
         Blade::component('component.task', 'task');
         Blade::component('component.card', 'card');
+
+        if(env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
